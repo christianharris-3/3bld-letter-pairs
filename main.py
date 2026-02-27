8d7227f57bc90a6ac5a487ce646586a481969723
Chris Harris <christian.harris-3@student.manchester.ac.uk>
2026-02-27 12:51:15 +0000

    changed file saving for smarter quiz

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
+import math
 import random
 
 import bcrypt
 import streamlit as st
-st.set_page_config(layout="wide")
+st.set_page_config(layout="wide", page_title="3BLD Letter Pairs")
 import json
 import os
+import copy
+from datetime import datetime
 
 LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWX"
+DATA_PATH = "data"
 
 class Funcer:
     def __init__(self, func, args):
         self.func = lambda: func(args)
 
 def get_filename():
-    return f"pairs_{st.session_state.get('username', '')}.json"
+    return f"{DATA_PATH}/pairs_{st.session_state.get('username', '')}.json"
 
 def load_data():
     if os.path.exists(get_filename()):
@@ -21,18 +25,30 @@ def load_data():
             data = json.load(f)
     else:
         data = {}
+
+    # convert datetime
+    for key in data:
+        data[key]["last_checked"] = datetime.fromisoformat(data[key]["last_checked"])
+
+    # fill out data
     for a in LETTERS:
         for b in LETTERS:
             pair = a+b
             if a != b and (pair not in data) and len(pair) == 2:
-                data[pair] = ""
+                data[pair] = {
+                    "word": "",
+                    "last_confidence": 0,
+                    "last_checked": datetime.now(),
+                }
     return data
 
 def save_data(data):
     filtered_data = {}
     for key, val in data.items():
-        if val != "":
-            filtered_data[key] = val
+        if val["word"] != "":
+            filtered_data[key] = copy.deepcopy(val)
+            filtered_data[key]["last_checked"] = filtered_data[key]["last_checked"].isoformat()
+
     with open(get_filename(), "w") as f:
         json.dump(filtered_data, f, indent="  ")
 
@@ -40,15 +56,15 @@ def view_letter(pair, data, con):
     with con.form("Edit Word Form", border=False):
         st.markdown(f"## Pair {pair}")
 
-        word = data[pair]
-        if not (word == "" or word is None):
-            st.markdown(f"Word is: {word}")
+        dict_ = data[pair]
+        if dict_["word"] != "":
+            st.markdown(f"Word is: {dict_['word']}")
 
         new_word = st.text_input("Enter Word", key="word_input")
         submitted = st.form_submit_button("Save")
 
         if submitted:
-            data[pair] = new_word
+            data[pair]["word"] = new_word
             save_data(data)
             st.toast(f"Pair {pair} set to {new_word}")
             st.session_state["clear_word_input"] = True
@@ -87,44 +103,102 @@ def letter_search(data):
 
     make_grid(st.container(border=True))
 
+def pick_pair(available_pairs):
+    data = []
+    for key, dict_ in available_pairs.items():
+
+        time_since = (datetime.now().timestamp() - dict_["last_checked"].timestamp()) / 60 / 60 / 24
+        value = math.log2(time_since) - dict_["last_confidence"] + random.random()
+
+        data.append((
+            key,
+            value
+        ))
+
+    data.sort(key=lambda x: x[1], reverse=True)
+    return data[0][0]
+
 def letter_quiz(data):
     st.markdown("# Quiz")
 
     bag = {}
     for key, val in data.items():
-        if val != "":
+        if val["word"] != "":
             bag[key] = val
 
     if len(bag) < 2:
         st.markdown("No words given, can't quiz you on nothing!")
     else:
         if st.session_state.get("random_pair", None) is None:
-            st.session_state["random_pair"] = random.choice(list(bag.keys()))
+            st.session_state["random_pair"] = pick_pair(bag)
         del bag[st.session_state["random_pair"]]
 
         st.markdown(f"What is the word for {st.session_state['random_pair']}?")
-        if st.button("Show", shortcut="Space"):
-            st.markdown(f"Word is \"{data[st.session_state['random_pair']]}\"")
-            shown = True
+
+        if st.session_state.get("show_quiz_answer", None) is None:
+            st.session_state["show_quiz_answer"] = False
+
+        show_button = st.button("Show", shortcut="Space")
+        word_container = st.empty()
+        answer_text = f"Word is \"{data[st.session_state['random_pair']]['word']}\""
+        if show_button:
+            word_container.markdown(answer_text)
+            if st.session_state["show_quiz_answer"]:
+                data[st.session_state["random_pair"]]["last_checked"] = datetime.now()
+                save_data(data)
+                st.session_state["random_pair"] = pick_pair(bag)
+                st.session_state["show_quiz_answer"] = False
+                st.rerun()
+            st.session_state["show_quiz_answer"] = True
+
+        if not st.session_state["show_quiz_answer"]:
+            word_container.markdown(f"Word is -----")
         else:
-            st.markdown(f"Word is -----")
-            shown = False
-        if st.button("Next", shortcut = "Space" if shown else "Enter"):
-            st.session_state["random_pair"] = random.choice(list(bag.keys()))
-            st.rerun()
+            word_container.markdown(answer_text)
+
+        st.markdown("Select Confidence (1 is low, 3 is high)")
+
+        cols = st.columns([1, 1, 1, 2.5], width=300)
+
+        next_word = 0
+        if cols[0].button("1", shortcut = "1", width="stretch"):
+            next_word = 1
+        if cols[1].button("2", shortcut = "2", width="stretch"):
+            next_word = 2
+        if cols[2].button("3", shortcut = "3", width="stretch"):
+            next_word = 3
+        if cols[3].button("Next", shortcut = "Enter", width="stretch"):
+            next_word = 4
+
+        if next_word > 0:
+            if not st.session_state["show_quiz_answer"]:
+                word_container.markdown(answer_text)
+                st.session_state["show_quiz_answer"] = True
+            else:
+                if next_word < 4:
+                    data[st.session_state["random_pair"]]["last_confidence"] = next_word
+                data[st.session_state["random_pair"]]["last_checked"] = datetime.now()
+                save_data(data)
+                st.session_state["random_pair"] = pick_pair(bag)
+                st.session_state["show_quiz_answer"] = False
+                st.rerun()
+        # elif st.session_state["show_quiz_answer"]:
+            # word_container.markdown(f"Word is \"{data[st.session_state['random_pair']]['word']}\"")
+            # st.session_state["show_quiz_answer"] = True
 
 def enter_words(data):
     st.markdown("# Enter Words")
 
     unused_pairs = []
     for key, val in data.items():
-        if val == "":
+        if val["word"] == "":
             unused_pairs.append(key)
 
     st.markdown(f"There are {len(unused_pairs)} pairs without words")
-    pair = st.selectbox("Select Letter", unused_pairs)
+    if len(unused_pairs) > 0:
+        pair = st.selectbox("Select Letter", unused_pairs)
 
-    view_letter(pair, data, st.container(border=True))
+        view_letter(pair, data, st.container(border=True))
 
 def manage_files(data):
     st.markdown("# Files")
@@ -202,7 +276,7 @@ def login_page():
 
     login, register = st.tabs(["Login", "Register"])
 
-    accounts_file = "accounts.json"
+    accounts_file = f"{DATA_PATH}/accounts.json"
     if not os.path.exists(accounts_file):
         with open(accounts_file, "w") as f:
             json.dump([], f, indent="  ")
@@ -249,6 +323,8 @@ def login_page():
                 else:
                     st.error("Invalid Credentials")
 
+if not(os.path.exists(DATA_PATH+"/")):
+    os.mkdir(DATA_PATH)
 
 if st.session_state.get("authenticated", False):
     app()
